8943ae51ff2c97b3468be2df1a21efda9cc77039
AbhijitBorate-code <66520062+AbhijitBorate-code@users.noreply.github.com>
2024-11-18 07:47:04 +0000

    css formatted proeprly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { GeminiService } from './gemini.service';
@@ -7,33 +7,45 @@ import { GeminiService } from './gemini.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewChecked {
+  @ViewChild('chatContainer') private chatContainer!: ElementRef;
+
   message: any[] = [];
-  messageAI: any[] = [];
   myForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private geminiService: GeminiService) {}
+  constructor(private fb: FormBuilder, private geminiService: GeminiService) { }
 
   ngOnInit(): void {
     this.myForm = this.fb.group({
       text: [''],
     });
-    this.messageAI = [];
     this.geminiService.getMessageHistory().subscribe((mesg) => {
-      if(mesg != null )
-        {
-          this.messageAI.push(mesg);
-        }
+      if (mesg != null) {
+        this.message.push({ text: mesg, sender: 'ai' });;
+        this.scrollToBottom();
+      }
     });
+  }
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom() {
+    try {
+      this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
+    } catch (err) {
+      console.error('Scroll error:', err);
+    }
   }
 
   onSubmit() {
     const textValue = this.myForm.controls['text'].value.trim();
-
     if (textValue !== '') {
       console.log(textValue);
-      this.message.push(textValue);
-      this.geminiService.generateText(textValue);
+      this.message.push({ text: textValue, sender: 'user' });  // Add user message
+      this.geminiService.generateText(textValue)
+      this.scrollToBottom();
       this.myForm.reset();
     }
   }
